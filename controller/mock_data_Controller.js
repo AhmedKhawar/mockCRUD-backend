49f449526735b051f65fd025b9e4c3bfd7171ec8
Muhammad Ahmed Khawar <ahmedkhawar.400@gmail.com>
2026-09-14 21:00:21 +0500

    8.0

--- controller/mock_data_Controller.js
+++ controller/mock_data_Controller.js
@@ -78,7 +78,12 @@ export const mockAPI = async (req, res) => {
 
         // Fetch all records
         const records = await MockData.find(baseQuery);
-        return res.status(200).json(records.map((doc) => ({ id: doc._id, ...doc.data })));
+       return res.status(200).json({
+         records: records.map((doc) => ({
+          id: doc._id,
+          ...doc.data })),
+          count: records.length
+        });
       }
 
       case "POST": {
