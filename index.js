6c53a15d3b3faeab8cddc6a19a83546b18ebf533
Muhammad Ahmed Khawar <ahmedkhawar.400@gmail.com>
2026-09-25 23:44:00 +0500

    fix(cors): add vercel frontend domain to CORS origins

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ app.use(express.json())
 
 
 app.use(cors({
-  origin: ["https://www.mockcrud.xyz", "http://localhost:5173"],
+  origin: [
+    "https://www.mockcrud.xyz",
+    "https://mock-crud-blond.vercel.app",
+    "http://localhost:5173"
+  ],
   credentials: true
 }));
 
