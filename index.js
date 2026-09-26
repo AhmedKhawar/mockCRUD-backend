fea5101d2907384510f9eec1c1c3abdd62642525
Muhammad Ahmed Khawar <ahmedkhawar.400@gmail.com>
2026-09-26 17:55:29 +0500

    Testing PR 2

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(express.json())
 app.use(cors({
   origin: [
     "https://www.mockcrud.xyz",
+    "https://mock-crud-blond.vercel.app",
     "http://localhost:5173"
   ],
   credentials: true
